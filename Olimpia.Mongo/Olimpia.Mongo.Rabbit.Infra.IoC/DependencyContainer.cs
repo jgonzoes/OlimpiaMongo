767dbf7ce9ba99ad1f07329435168c5886d6abb5
Joaquin Gonzalez <jgonzoes@gmail.com>
2020-02-29 13:40:18 -0500

    Infta IoC basico

---
+++ Olimpia.Mongo/Olimpia.Mongo.Rabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Olimpia.Mongo.Domain.core.Bus;
+using Olimpia.Mongo.Rabbit.Infra.Bus;
+
+namespace Olimpia.Mongo.Rabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void Register(IServiceCollection services)
+        {
+            //Domain Bus
+            services.AddSingleton<IEventBus, RabbitMQBus>(sp =>
+            {
+                var scopeFactory = sp.GetRequiredService<IServiceScopeFactory>();
+                return new RabbitMQBus(sp.GetService<IMediator>(), scopeFactory);
+            });
+
+
+        }
+    }
+}
